4ae8e1e45ca05eb13dad6dfc4d5c3c1508cc9556
HeleneLevry <helenelevry@gmail.com>
2017-05-10 10:20:03 +0200

    enregistrement entité

--- src/AdminBundle/Controller/DefaultController.php
+++ src/AdminBundle/Controller/DefaultController.php
@@ -24,11 +24,11 @@ class DefaultController extends Controller
     $em->persist($compte);
     // Étape 2 : On « flush » tout ce qui a été persisté avant
     $em->flush();
-    // Reste de la méthode qu'on avait déjà écrit
-    if ($request->isMethod('POST')) {
-		$request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
-		return $this->redirect($this->generateUrl('oc_platform_view', array('id' => $advert->getId())));
-		}  
+    //// Reste de la méthode qu'on avait déjà écrit
+    //if ($request->isMethod('POST')) {
+		//$request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+		//return $this->redirect($this->generateUrl('oc_platform_view', array('id' => $advert->getId())));
+		//}  
     return $this->render('AdminBundle:Default:index.html.twig');
     }
 }
