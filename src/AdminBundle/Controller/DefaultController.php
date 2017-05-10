bd4538a5a9bddb7b753e8e4db4a327f047fdbf76
HeleneLevry <helenelevry@gmail.com>
2017-05-10 10:07:03 +0200

    enregistrement entité

--- src/AdminBundle/Controller/DefaultController.php
+++ src/AdminBundle/Controller/DefaultController.php
@@ -11,52 +11,24 @@ class DefaultController extends Controller
         return $this->render('AdminBundle:Default:index.html.twig');
     }
     
-    //public function addAction(Request $request)
-    
-    //{
-    
-    //// Création de l'entité
-    
-    //$advert = new Advert();
-    
-    //$advert->setTitle('Recherche développeur Symfony2.');
-    
-    //$advert->setAuthor('Alexandre');
-    
-    //$advert->setContent("Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…");
-    
-    //// On peut ne pas définir ni la date ni la publication,
-    
-    //// car ces attributs sont définis automatiquement dans le constructeur
-    
-    
-    //// On récupère l'EntityManager
-    
-    //$em = $this->getDoctrine()->getManager();
-    
-    
-    //// Étape 1 : On « persiste » l'entité
-    
-    //$em->persist($advert);
-    
-    
-    //// Étape 2 : On « flush » tout ce qui a été persisté avant
-    
-    //$em->flush();
-    
-    
-    //// Reste de la méthode qu'on avait déjà écrit
-    
-    //if ($request->isMethod('POST')) {
-    
-    //$request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
-    
-    //return $this->redirect($this->generateUrl('oc_platform_view', array('id' => $advert->getId())));
-    
-    //}
-    
-    
-    //return $this->render('OCPlatformBundle:Advert:add.html.twig');
-    
-    //}
+    public function addAction(Request $request)
+    {
+    // Création de l'entité
+    $compte = new Compte();
+    $compte->setEmail(monemail@mail.com);
+    $compte->setUser(monuser);
+    $compte->setPassword(monpassword);
+    // On récupère l'EntityManager
+    $em = $this->getDoctrine()->getManager();
+    // Étape 1 : On « persiste » l'entité
+    $em->persist($compte);
+    // Étape 2 : On « flush » tout ce qui a été persisté avant
+    $em->flush();
+    // Reste de la méthode qu'on avait déjà écrit
+    if ($request->isMethod('POST')) {
+		$request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+		return $this->redirect($this->generateUrl('oc_platform_view', array('id' => $advert->getId())));
+		}  
+    return $this->render('AdminBundle:Default:index.html.twig');
+    }
 }
