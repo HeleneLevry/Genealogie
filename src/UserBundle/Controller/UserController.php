bd699c5040abc3c1360dda03ceefa3841d018cc3
MarineLohezic <lohezic.marine@gmail.com>
2017-05-11 17:44:48 +0200

    liens menu

--- src/UserBundle/Controller/UserController.php
+++ src/UserBundle/Controller/UserController.php
@@ -21,4 +21,19 @@ class UserController extends Controller
 	{		
 		return $this->render('UserBundle:User:menu.html.twig');
 	}
+	
+	public function ajouterAction()
+	{
+		return $this->render('UserBundle:User:ajouter.html.twig');
+	}
+	
+	public function modifierAction()
+	{
+		return $this->render('UserBundle:User:modifier.html.twig');
+	}
+	
+	public function supprimerAction()
+	{
+		return $this->render('UserBundle:User:supprimer.html.twig');
+	}
 }
