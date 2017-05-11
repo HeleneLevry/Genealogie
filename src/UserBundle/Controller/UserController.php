e8818646a2c13f5c03b08d18143fab644467c405
MarineLohezic <lohezic.marine@gmail.com>
2017-05-11 17:16:57 +0200

    permission modifié

--- src/UserBundle/Controller/UserController.php
+++ src/UserBundle/Controller/UserController.php
@@ -8,10 +8,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
-{
+{	
+	public function indexAction(){
+		return $this->render('UserBundle:User:index.html.twig');
+	}
+
 	public function accueilAction(){
-		
-		//return new Response("Accueil");
 		return $this->render('UserBundle:User:accueil.html.twig');
 	}	
+	
+	public function menuAction()//transmis via le render() depuis la vue /app
+	{		
+		return $this->render('UserBundle:User:menu.html.twig');
+	}
 }
