e2d9d2f66437cf0fcb77beee37fd9cce40866d90
MarineLohezic <lohezic.marine@gmail.com>
2017-05-16 16:27:15 +0200

    ajouter une patho a la bdd

--- src/AdminBundle/Controller/AdminController.php
+++ src/AdminBundle/Controller/AdminController.php
@@ -6,41 +6,41 @@ namespace AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+
 use AdminBundle\Entity\Banque_Patho;
+use AdminBundle\Form\Banque_PathoType;
 
 class AdminController extends Controller
 {	
 	public function ajouter_pathoAction()
 	{
-		$new_patho= new Banque_Patho();
+		$new_patho= new Banque_Patho;
 		
-		$formBuilder= $this->createFormBuilder($new_patho);
-		
-		$formBuilder
-			->add ('nomPathologie', 'text');
-			
 		//On genere le formulaire
-		 $form=$formBuilder->getForm();
+		 $form=$this->createForm(new Banque_PathoType, $new_patho);
 		 
 		 //On récupère la requête
 		 $request = $this->get('request');
-		 
+
 		 //On verifie qu'elle est de type POST
-		 if ($request->getMethod() == 'POST'){
+		 
+		 if (($this->get('request')->getMethod()) == 'POST'){
+			 echo('apres if POST');
 			 $form->bind($request);
+			 echo('avant if VALID');
 			 
-			// if ($form->isValid()){
-				 
+			 if ($form->isValid()){
+				 echo('apres if VALID');
 				 //On enregistre notre objet dans la bdd
 				 $em = $this->getDoctrine()->getManager();
 				 $em->persist($new_patho);
 				 $em->flush();
 				 
 			return $this->redirect($this->generateUrl('user_connected_index'));
-			//}
+			}
 		}
 		
-		return $this->render('AdminBundle:Admin:ajouter_patho.html.twig',array('form' => $form->createView(),));
+		return $this->render('AdminBundle:Admin:ajouter_patho.html.twig',array('form' => $form->createView()));
 	}
 	
 	
